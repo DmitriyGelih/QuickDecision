d10d48f44fe33cce9a88e2559e546424d47aaee6
eduardr <eduardr@softwareplanet.uk.com>
2014-01-13 10:06:57 +0200

    add content to tabs

--- app/assets/javascripts/app/controllers/tabs_controller.js
+++ app/assets/javascripts/app/controllers/tabs_controller.js
@@ -1,17 +1,126 @@
 angular.module('plunker', ['ui.bootstrap']);
 var tabsController = function ($scope) {
-	var project = "Тахометр",
-		alternative = 'альтернатива 1',
-		alternative2 = 'альтернатива 2',
-		criteria1 = 'критерий 1',
-		criteria2 = 'критерий 2'
+    $scope.items = [
+        { name: 'first' },
+        { name: 'second' }
+    ];
+    $scope.evaluate;
 
-	$scope.tabs = [
-		{ title:"Проект", content:[project] , active:true},
-		{  title:"Альтернативи", content:[alternative, alternative2] },
-		{  title:"Критерии", content:[criteria1, criteria2] },
-		{  title:"Оценивать", content:[] },
-		{  title:"Результат", content:[] }
-	];
+    // from server
+    $scope.project = "Покупка авто";
+    $scope.alternatives = ['BMW', 'Mercedes', 'KIA'];
+    $scope.criteriaWeights = [
+        { name: 'Цвет', weight: 1 },
+        { name: 'Цена', weight: 1 },
+        { name: 'Гарантия', weight: 1 },
+        { name: 'Расход топлива', weight: 1 }
+    ];
+
+//        $scope.directRating = function () {
+    $scope.criterias = []; //list color, petrol
+    angular.forEach($scope.criteriaWeights, function (value, _) {
+        $scope.criterias.push(value.name)
+    });
+
+    var criteriaEvaluates = []; // color, price
+    for (var i = 0; i < $scope.criteriaWeights.length; i++) {
+        for (var j = i + 1; j < $scope.criteriaWeights.length; j++) {
+            criteriaEvaluates.push(
+                { leftName: $scope.criteriaWeights[i].name, leftWeight: $scope.criteriaWeights[i].weight, rightName: $scope.criteriaWeights[j].name, rightWeight: $scope.criteriaWeights[j].weight, slider: 0}
+            )
+        };
+    };
+
+    var alternativeEvaluates = []; // bmw
+    angular.forEach($scope.criterias, function (criteria) {
+        var h = {};
+        h[criteria] = [];
+        angular.forEach($scope.alternatives, function (alternative) {
+            h[criteria].push({ name: alternative, weight: 0})
+        });
+
+        alternativeEvaluates.push(h);
+    });
+
+    $scope.alternativeEvaluates = alternativeEvaluates; // color, price
+    $scope.criteriaEvaluates = criteriaEvaluates; // bmw
+
+    $scope.result = function () {
+        angular.forEach($scope.criteriaEvaluates, function (value) {
+            value.leftWeight = 1;
+            value.rightWeight = 1;
+
+            if (!value.slider) return;
+
+            if (value.slider > 0) {
+                value.rightWeight = (value.slider)
+            }
+            else {
+                value.leftWeight = (value.slider * -1)
+            }
+            ;
+        });
+
+        $scope.resultSeries = [
+            {
+                name: 'Цвет',
+                data: [1, 2, 1]
+            },
+            {
+                name: 'Расход топлива',
+                data: [1, 3, 2]
+            },
+            {
+                name: 'Гарантия',
+                data: [3, 3, 2]
+            },
+            {
+                name: 'Цена',
+                data: [2, 1, 3]
+            }
+        ];
+        drawChart();
+    };
+//        };
+    var drawChart = function () {
+        $(function () {
+            $('#container').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: $scope.project
+                },
+                xAxis: {
+                    categories: $scope.alternatives
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Результат'
+                    }
+                },
+                legend: {
+
+                    backgroundColor: '#FFFFFF',
+                    reversed: true
+                },
+                plotOptions: {
+                    series: {
+                        stacking: 'normal'
+                    }
+                },
+                series: $scope.resultSeries
+            });
+        });
+    };
+
+    $scope.tabs = [
+        { title: "Проекты", content: [$scope.project], active: true},
+        { title: "Альтернативы", content: $scope.alternatives  },
+        { title: "Критерии", content: ['Цвет', 'Цена', 'Расход топлива', 'Гарантия'] },
+        { title: "Оценить", content: [] },
+        { title: "Результат", content: [] }
+    ];
 //    $scope.navType = 'pills';
 };
--- app/assets/javascripts/app/controllers/welcome_controller.js
+++ app/assets/javascripts/app/controllers/welcome_controller.js
@@ -7,33 +7,5 @@ angular.module('quickDecision.controllers', []).
           console.log(item)
         };
 
-        $scope.items = [{ name: 'first' }, { name: 'second' }];
-
-        $scope.evaluate;
-        $scope.project = "Cars";
-        $scope.slider = 0.0;
-
-        $scope.pairwiseComparisons = function () {
-            console.log("click on the pairwiseComparisons")
-        }
-
-        $scope.directRating = function () {
-            var alternaties = [
-               { name: 'BMW', weight: 0.5 },
-               { name: 'KIA', weight: 0.2 },
-               { name: 'VAZ', weight: 0.2 },
-               { name: 'ZAZ', weight: 0.1 }
-            ];
-
-            var evaluates = [];
-            for (var i = 0; i < alternaties.length; i++) {
-                for (var j = i + 1; j < alternaties.length; j++) {
-                    evaluates.push(
-                       { leftName: alternaties[i].name, left_Weight: alternaties[i].weight, rightName: alternaties[j].name, rightWeight: alternaties[j].weight}
-                    )
-                };
-            };
-
-            $scope.evaluate = evaluates;
-        };
+        $scope.items = [{ name: 'Покупка авто' }];
     });
